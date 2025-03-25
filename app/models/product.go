534270e4150346df76262657e0fc62f4641ecdb2
Nanda Rahmat Nurdiyan <nandarahmatnurdiyan@gmail.com>
2025-03-25 18:07:31 +0700

    add: pagination

--- app/models/product.go
+++ app/models/product.go
@@ -28,3 +28,24 @@ type Product struct {
 	UpdatedAt        time.Time
 	DeletedAt        gorm.DeletedAt
 }
+
+func (p *Product) GetProducts(db *gorm.DB, perPage int, page int) (*[]Product, int64, error) {
+	var err error
+	products := []Product{}
+	var count int64
+
+	err = db.Debug().Model(&Product{}).Count(&count).Error
+	if err != nil {
+		return nil, 0, err
+	}
+
+	offset := (page - 1) * perPage
+
+	err = db.Debug().Model(&Product{}).Order("created_at desc").Limit(perPage).Offset(offset).Find(&products).Error
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return &products, count, nil
+}
+
--- app/server.go
+++ app/server.go
@@ -2,133 +2,39 @@ package app
 
 import (
 	"flag"
-	"fmt"
 	"log"
-	"net/http"
 	"os"
 
-	seeder "github.com/nandarahmat/database/seeders"
-
-	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
-	"github.com/urfave/cli"
-	"gorm.io/driver/mysql"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	"github.com/nandarahmat/app/controllers"
 )
-
-type Server struct {
-	DB *gorm.DB
-	Router *mux.Router
-}
-
-type AppConfig struct {
-	AppName string
-	AppEnv string
-	AppPort string
-}
-
-type DBConfig struct {
-	DBHost string
-	DBUser string
-	DBPassword string
-	DBName string
-	DBPort string
-	DBDriver string
-}
-
-func (server *Server) Initialize(appConfig AppConfig, dbConfig DBConfig) {
-	fmt.Println("Welcome to " + appConfig.AppName)
-
-	server.initializeRoutes()
-}
-
-func (server *Server) Run(addr string) {
-	fmt.Printf("Listening to port %s", addr)
-	log.Fatal(http.ListenAndServe(addr, server.Router))
-}
-func (server *Server) initializeDB(dbConfig DBConfig) {
-	var err error
-	if dbConfig.DBDriver == "mysql" {
-		dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbConfig.DBUser, dbConfig.DBPassword, dbConfig.DBHost, dbConfig.DBPort, dbConfig.DBName)
-		server.DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	} else {
-		dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta", dbConfig.DBHost, dbConfig.DBUser, dbConfig.DBPassword, dbConfig.DBName, dbConfig.DBPort)
-		server.DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	}
-
-	if err != nil {
-		panic("Failed on connecting to the database server")
-	}
-}
-func (server *Server) dbMigrate() {
-	for _, model := range RegisterModels() {
-		err := server.DB.Debug().AutoMigrate(model.Model)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	fmt.Println("Database migrated successfully.")
-}
-
-func (server *Server) initCommands(config AppConfig, dbConfig DBConfig) {
-	server.initializeDB(dbConfig)
-
-	cmdApp := cli.NewApp()
-	cmdApp.Commands = []cli.Command{
-		{
-			Name: "db:migrate",
-			Action: func(c *cli.Context) error {
-				server.dbMigrate()
-				return nil
-			},
-		},
-		{
-			Name: "db:seed",
-			Action: func(c *cli.Context) error {
-				err := seeder.DBSeed(server.DB)
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				return nil
-			},
-		},
-	}
-
-	err := cmdApp.Run(os.Args)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
 
 
 func getEnv(key, fallback string) string {
 	if value, ok := os.LookupEnv(key); ok {
 		return value
-	} 
+	}
 
 	return fallback
 }
 
-func Run(){
-	var server = Server{}
-	var appConfig = AppConfig{}
-	var dbConfig = DBConfig{}
+func Run() {
+	var server = controllers.Server{}
+	var appConfig = controllers.AppConfig{}
+	var dbConfig = controllers.DBConfig{}
 
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatalf("Error getting env, %v", err)
-	} 
+		log.Fatalf("Error on loading .env file")
+	}
 
-	appConfig.AppName = getEnv("APP_NAME", "Golang Toko")
+	appConfig.AppName = getEnv("APP_NAME", "GoToko")
 	appConfig.AppEnv = getEnv("APP_ENV", "development")
 	appConfig.AppPort = getEnv("APP_PORT", "9000")
+	appConfig.AppURL = getEnv("APP_URL", "http://localhost:9000")
 
 	dbConfig.DBHost = getEnv("DB_HOST", "localhost")
-	dbConfig.DBUser = getEnv("DB_USER", "root")
+	dbConfig.DBUser = getEnv("DB_USER", "user")
 	dbConfig.DBPassword = getEnv("DB_PASSWORD", "password")
 	dbConfig.DBName = getEnv("DB_NAME", "dbname")
 	dbConfig.DBPort = getEnv("DB_PORT", "5432")
@@ -136,11 +42,11 @@ func Run(){
 
 	flag.Parse()
 	arg := flag.Arg(0)
+
 	if arg != "" {
-		server.initCommands(appConfig, dbConfig)
+		server.InitCommands(appConfig, dbConfig)
 	} else {
 		server.Initialize(appConfig, dbConfig)
-		server.Run(": " + appConfig.AppPort)
+		server.Run(":" + appConfig.AppPort)
 	}
-
 }
